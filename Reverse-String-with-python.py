4a11cc1816c4da9baad924787989526aa2d80e1f
Ali Najafi <76534541+Ali9721@users.noreply.github.com>
2025-07-09 09:42:56 -0700

    Reverse-String-with-python
    1. Get input for a string
    
    2. Define reversed_text and a condition that cause reverse string.
    
    3. Output the result

---
+++ Reverse-String-with-python.py
@@ -0,0 +1,8 @@
+# Get input for a string
+text = input("Please enter a string: ")
+
+# Define reversed_text and a condition that cause reverse string.
+reversed_text = text[::-1]
+
+# Output the result
+print(f"Reversed string:  {reversed_text}")
